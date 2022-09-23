5b731f92483bbfdf388e159abd25d0083c1d7818
kangzuyao <351658471@qq.com>
2022-09-23 15:12:53 +0800

    提交

---
+++ src/views/profile/components/ExCard.vue
@@ -0,0 +1,78 @@
+<template>
+    <el-row :gutter="24" style="width:100%">
+      <el-col :span="12" v-for="item in exList" :key="item">
+        <el-card shadow="hover">
+          <div class="card-header">
+            <div>{{item.title}}</div>
+            <div style="font-size: 20px">
+              <View style="width: 1em; height: 1em; margin-right: 8px"  @click="handleView"/>
+              <Edit style="width: 1em; height: 1em; margin-right: 8px" @click="handleEdit"/>
+              <Delete style="width: 1em; height: 1em; margin-right: 8px" @click="handleDelete" />
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="10">
+        <el-button type="primary" style="margin-top:10px" @click="openAddDialog">添加经历</el-button>
+      </el-col>
+       <el-dialog
+          v-model="dialogVisible"
+          title="经历"
+          width="40%"
+        >
+          <el-form :model="exForm.data" label-width="80px">
+            <el-form-item label="标题" prop="" >
+              <el-input style="width:90%;margin-bottom:10px" v-model="exForm.data.title"></el-input>
+            </el-form-item>
+            <el-form-item label="二级标题">
+              <el-input style="width:90%;margin-bottom:10px" v-model="exForm.data.secondTitle"></el-input>
+            </el-form-item>
+            <el-form-item label="内容">
+              <el-input style="width:90%;margin-bottom:10px" v-model="exForm.data.content" type="textarea"></el-input>
+            </el-form-item>
+          </el-form>
+          <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="dialogVisible = false">Cancel</el-button>
+              <el-button type="primary" @click="dialogVisible = false"
+                >Confirm</el-button
+              >
+            </span>
+          </template>
+        </el-dialog>
+  </el-row>
+</template>
+
+<script setup>
+import {reactive, ref} from 'vue'
+const props = defineProps({
+  exList:{
+    type:Array,
+    default:()=>([])
+  }
+})
+const dialogVisible = ref(false)
+const exForm = reactive({
+  data:{
+    title:'',
+    secondTitle:'',
+    content:''
+  }
+})
+const openAddDialog= ()=> {
+  dialogVisible.value = true
+}
+const handleView= ()=>{
+  dialogVisible.value = true
+}
+const handleDelete= () => {
+  console.log('handleDelete');
+}
+const handleEdit= () => {
+  dialogVisible.value = true
+}
+</script>
+
+<style>
+
+</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="out-box">
     <div class="profile">
-      <el-form :model="form" label-width="100px">
+      <el-form :model="formData" label-width="100px">
         <el-form-item label="个人简介">
-          <el-input type="textarea"/>
+          <el-input type="textarea" v-model="formData.obj.intor"/>
         </el-form-item>
-        <el-form-item label="经历">
-            <el-row :gutter="24" style="width:100%">
-              <el-col :span="8">
-                <el-card shadow="hover"> Always </el-card>
-              </el-col>
-              <el-col :span="8">
-                <el-card shadow="hover"> Hover </el-card>
-              </el-col>
-              <el-col :span="8">
-                <el-button type="primary" @click="openAddDialog">添加经历</el-button>
-              </el-col>
-            </el-row>
+        <el-form-item label="教育经历">
+            <ExCard :ex-list="infoObj.exList"/>
+        </el-form-item>
+        <el-form-item label="工作经历">
+            <ExCard :ex-list="infoObj.workList"/>
         </el-form-item>
         <el-form-item label="标签">
             <el-tag
@@ -44,22 +37,50 @@
         </el-form-item>
     </el-form>
     </div>
-    <div class="skill">
-      <div>技能1</div>
-    </div>
+    <div class="save-box">
+    <el-button type="primary">保存</el-button>
   </div>
+  </div>
+  
+  
 </template>
 
 <script setup>
-import {ref, nextTick} from 'vue'
-const openAddDialog = () => {
-
-}
+import {ref, nextTick, reactive} from 'vue'
+import ExCard from './components/ExCard.vue'
+const formData = reactive({
+  obj:{
+    intor:''
+  }
+})
+const openAddDialog = () => {}
 const inputValue = ref('')
 const dynamicTags = ref(['Tag 1', 'Tag 2', 'Tag 3'])
 const inputVisible = ref(false)
 const InputRef = ref()
+const infoObj = reactive({
+  intor:'',
+  exList:[
+    {
+      title:'厦门工学院-本科',
+      secondTitle:'2015.09 - 2019.06',
 
+    },
+    {
+      title:'厦门工学院-本科',
+      secondTitle:'2015.09 - 2019.06',
+
+    }
+  ],
+  workList:[
+    {
+      title:'xx世纪科技有限公司',
+      secondTitle:'前端开发',
+    }
+  ],
+  tagList:[],
+  skillList:[]
+})
 const handleClose = (tag) => {
   dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1)
 }
@@ -83,11 +104,9 @@ const handleInputConfirm = () => {
 <style>
 .out-box{
   width: 100%;
-  display: flex;
-  justify-content: space-between;
 }
 .profile{
-  width: 55%;
+  /* width: 55%; */
 }
 .skill{
   width: 40%;
@@ -97,5 +116,13 @@ const handleInputConfirm = () => {
 }
 .tag-input{
   width: 100px;
+}
+.card-header{
+  display: flex;
+  justify-content: space-between;
+}
+.save-box{
+  display: flex;
+  justify-content: center;
 }
 </style>
